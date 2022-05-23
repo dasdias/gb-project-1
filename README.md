c89a8cbadbb317cdd1907b85e5ed63373010a479
Alexender <dasdias@yandex.ru>
2022-05-23 22:43:21 +0300

    project development 01

---
+++ README.md
@@ -0,0 +1,2 @@
+# gb-project-1
+[Посмотреть страницу на Github Pages](https://dasdias.github.io/gb-project-1/)
--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    {id: 1, title: 'Notebook', price: 2000, img: 'laptop.png'},
+    {id: 2, title: 'Mouse', price: 20, img: 'mouse.png'},
+    {id: 3, title: 'Keyboard', price: 200, img: 'keyboard.png'},
+    {id: 4, title: 'Gamepad', price: 50, img: 'gamepad.png'},
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+const renderProduct = ({title, price, img, id}) => {
+    return `<div class="product-item" data-id="${id}">
+                <img src="images/${img}" alt="${title}">
+                <h3 class="product-item__title">${title}</h3>
+                <p>Цена: ${price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
+
+// Эти запятые выводятся из массива который мы вставляем в DOM элемент
+// Чтобы убрать запятые можно воспользоваться методом join('') без разделителя  
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title,item.price));
-    console.log(productsList);
+    const productsList = list.map(item => renderProduct({
+        title: item.title,
+        price: item.price,
+        img: item.img,
+        id: item.id
+    })).join('');
     document.querySelector('.products').innerHTML = productsList;
 };
 
